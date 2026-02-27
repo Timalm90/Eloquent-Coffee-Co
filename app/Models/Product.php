4abbb90a2babd4c384b2a95fee4c73578e6e118e
Timalm90 <timalm90@gmail.com>
2026-02-27 12:30:30 +0100

    Updated Product.php to include images. Added images.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,19 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'country_id', 'region_id', 'suffix_id', 'roast_id', 'type_id', 'in_stock'];
+    protected $fillable = [
+        'name',
+        'country_id',
+        'region_id',
+        'suffix_id',
+        'roast_id',
+        'type_id',
+        'in_stock'
+    ];
+
+    protected $casts = [
+        'in_stock' => 'boolean',
+    ];
 
     public function origin()
     {
@@ -30,5 +42,27 @@ class Product extends Model
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute()
+    {
+        if (!$this->type) {
+            return '/images/default.png';
+        }
+
+        $typeName = strtolower(str_replace(' ', '_', $this->type->type));
+
+        $path = public_path("images/$typeName");
+
+        $files = glob($path . '/*.png');
+
+        if (!$files) {
+            return '/images/default.png';
+        }
+
+        $file = $files[array_rand($files)];
+
+        return str_replace(public_path(), '', $file);
     }
 }
