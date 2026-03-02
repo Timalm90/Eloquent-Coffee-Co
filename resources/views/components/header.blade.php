21f1b0250a3c381cfcd3f4fc969b2450b8179c95
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-03-02 13:12:37 +0100

    Toggled login/logout if guest or auth

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,8 +3,12 @@
     <div><a href="/" class="text-blue-500 hover:underline">HOME</a></div>
     <div class="navLinks">
         <a href="/dashboard" class="text-blue-500 hover:underline">Dashboard</a>
+        @guest
         <a href=" {{ route('login') }}" class="text-blue-500 hover:underline">Login</a>
+        @endguest
+        @auth
         <a href="/logout" class="text-blue-500 hover:underline">Logout</a>
+        @endauth
     </div>
 </nav>
 
