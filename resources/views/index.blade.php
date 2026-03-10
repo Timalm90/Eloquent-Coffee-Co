282143f7a4f82c6e8bf4c282fdeeae45e5a163be
Timalm90 <timalm90@gmail.com>
2026-03-10 16:02:50 +0100

    added favicons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/favicon-light.ico" media="(prefers-color-scheme: light)">
+    <link rel="icon" href="/favicon-dark.ico" media="(prefers-color-scheme: dark)">
     <title>Products - Eloquent Coffee Co</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/favicon-light.ico" media="(prefers-color-scheme: light)">
+    <link rel="icon" href="/favicon-dark.ico" media="(prefers-color-scheme: dark)">
     <title>Login - Eloquent Coffee Co</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
